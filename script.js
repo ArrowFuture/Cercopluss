4be1c48acc40ff5ddb2408541e80f24e9d365206
ArrowFuture <leonardofvtvre@gmail.com>
2024-09-03 09:56:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Inicialización de Bootstrap Carousel
+    $('.carousel').carousel({
+        interval: 3000 // Cambia de slide cada 3 segundos
+    });
+});
